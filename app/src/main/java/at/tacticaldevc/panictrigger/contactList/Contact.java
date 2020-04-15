33c502b4d2f4702ef4688e6ccad62f2a9ac05c4e
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-15 17:55:00 +0530

    base app

---
+++ app/src/main/java/at/tacticaldevc/panictrigger/contactList/Contact.java
@@ -0,0 +1,12 @@
+package at.tacticaldevc.panictrigger.contactList;
+
+public class Contact {
+
+    public String name, number, groupID;
+    public Contact(String name, String number, String groupID)
+    {
+        this.name = name;
+        this.number = number;
+        this.groupID = groupID;
+    }
+}
